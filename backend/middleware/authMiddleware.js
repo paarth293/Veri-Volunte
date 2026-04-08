f884811a090162d651541e76253f1ab62a002ca7
paarth293 <i.m.paarthgupta@gmail.com>
2026-04-09 00:58:54 +0530

    added the decode token feature

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,11 +1,28 @@
-const admin = require("../config/firebase");
+const admin = require('../config/firebase');
 
-const verification = async(req, res, next) => {
-    const authHeader = req.headers.authorization;
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ 
+      error: 'Unauthorized', 
+      message: 'No token provided or invalid format.' 
+    });
+  }
+  const token = authHeader.split(' ')[1];
 
-    if(!authHeader || !authHeader.startWith('Bearer')) {
-        return res.status(401).json({
-            errro: 'Unauthorized',
-            message: 'No token provided or invalid format'
-        });
-}
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+
+  } catch (error) {
+    console.error('Error verifying Firebase ID token:', error);
+    return res.status(401).json({ 
+      error: 'Unauthorized', 
+      message: 'Invalid or expired token.' 
+    });
+  }
+};
+
+module.exports = { verifyToken };
